c0b446d46fc797303ac47a05918f914bc64153b3
aleupons <apahoquei@gmail.com>
2021-07-05 20:11:16 +0200

    Middleware get línia: per 1 línia torna totes les estacions + gestió errors + refactorització

--- api/tmb.js
+++ api/tmb.js
@@ -1,23 +1,35 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
 
-const urlLinies = process.env.URL_API;
 const appId = process.env.APP_ID;
 const appKey = process.env.APP_KEY;
+const urlLinies = `${process.env.URL_API}?app_id=${appId}&app_key=${appKey}`;
+const urlEstacions = (codiLinia) =>
+  `${process.env.URL_API}${codiLinia}/estacions/?app_id=${appId}&app_key=${appKey}`;
 
 const getLinies = async () => {
-  const response = await fetch(
-    `${urlLinies}?app_id=${appId}&app_key=${appKey}`
-  );
+  const response = await fetch(urlLinies);
   if (response) {
-    const linies = await response.json();
+    const { features: linies } = await response.json();
     return linies;
   } else {
-    console.log("Error de connexión");
-    return -1;
+    console.log("Error de connexió");
+    process.exit(0);
+  }
+};
+
+const getEstacions = async (codiLinia) => {
+  const response = await fetch(urlEstacions(codiLinia));
+  if (response) {
+    const { features: estacions } = await response.json();
+    return estacions.map((estacio) => estacio);
+  } else {
+    console.log("Error de connexió");
+    process.exit(0);
   }
 };
 
 module.exports = {
   getLinies,
+  getEstacions,
 };
--- server.js
+++ server.js
@@ -1,41 +1,97 @@
 const express = require("express");
-const { getLinies } = require("./api/tmb");
+const morgan = require("morgan");
+const { getLinies, getEstacions } = require("./api/tmb");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-  console.log("Servidor actiu");
+  console.log(`Servidor actiu al port ${port}`);
 });
 
 server.on("error", (err) => {
   console.log(err.message);
   if (err.code === "EADDRINUSE") {
-    console.log("Port ocupat");
+    console.log(`El port ${port} està ocupat`);
   }
 });
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/metro/lineas", async (req, res, next) => {
-  const { features: liniesApi } = await getLinies();
-  const linies = [];
-  const linia = {};
-  for (const { properties: liniaBuscada } of liniesApi) {
-    linia.id = liniaBuscada.CODI_LINIA;
-    linia.linea = liniaBuscada.NOM_LINIA;
-    linia.descripcion = liniaBuscada.DESC_LINIA;
-    linies.push(linia);
+  try {
+    const liniesApi = await getLinies();
+    const linies = liniesApi.map(({ properties: liniaBuscada }) => ({
+      id: liniaBuscada.CODI_LINIA,
+      linea: liniaBuscada.NOM_LINIA,
+      descripcion: liniaBuscada.DESC_LINIA,
+    }));
+    res.json(linies);
+  } catch (error) {
+    if (error.code === "ECONNREFUSED") {
+      error.message = "Error al connectar amb TMB";
+    }
+    res.status(500).json({ error: true, misatge: error.message });
   }
-  res.send(JSON.stringify(linies));
 });
 
-app.get("/metro/linea/", (req, res, next) => {
-  res.send({
-    linea: "L2",
-    descripcion: "Descripción de la línea",
-    paradas: [
-      {
-        id: "x",
-        nombre: "Nombre parada",
-      },
-    ],
-  });
+app.get("/metro/linea/:linia/", async (req, res, next) => {
+  try {
+    const liniesApi = await getLinies();
+    const { properties: liniaTrobada } = liniesApi.find(
+      (liniaBuscada) =>
+        liniaBuscada.properties.NOM_LINIA.toLowerCase() ===
+        req.params.linia.toLowerCase()
+    );
+    if (liniaTrobada === undefined) {
+      throw new Error("Aquesta línia no existeix");
+    }
+    const estacionsApi = await getEstacions(liniaTrobada.CODI_LINIA);
+    const linia = {
+      linea: liniaTrobada.NOM_LINIA,
+      descripcion: liniaTrobada.DESC_LINIA,
+      paradas: estacionsApi.map((estacioBuscada) => ({
+        id: estacioBuscada.properties.CODI_ESTACIO_LINIA,
+        nombre: estacioBuscada.properties.NOM_ESTACIO,
+      })),
+    };
+    res.json(linia);
+  } catch (error) {
+    console.log(error);
+    if (error.code === "ECONNREFUSED") {
+      error.message = "Error al connectar amb TMB";
+    }
+    res.status(500).json({ error: true, missatge: error.message });
+  }
+});
+
+app.put("*", (req, res, next) => {
+  res
+    .status(403)
+    .json({ error: true, mensaje: "Te pensabas que podías jaquearme" });
+});
+
+app.post("*", (req, res, next) => {
+  res
+    .status(403)
+    .json({ error: true, mensaje: "Te pensabas que podías jaquearme" });
+});
+
+app.delete("*", (req, res, next) => {
+  res
+    .status(403)
+    .json({ error: true, mensaje: "Te pensabas que podías jaquearme" });
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: true, missatge: "Aquesta pàgina no existeix" });
+});
+
+app.use((err, req, res, next) => {
+  const codi = err.codi || 500;
+  const missatge = err.codi
+    ? err.message
+    : { error: true, missatge: "Error general" };
+  res.status(codi).send(missatge);
 });
